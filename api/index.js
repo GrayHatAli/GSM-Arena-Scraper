dce15663110faadb777a1e3b9f0d45ac3d4d840c
GrayHatAli <grayhatali@gmail.com>
2025-11-13 18:03:18 +0330

    fix(api): ensure server instance is created before handling requests
    - Added logic to initialize the server instance if it doesn't already exist, ensuring the Express app is properly set up to handle incoming requests.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,14 @@ import { ScraperAPI } from '../src/api/server.js';
 const apiInstance = new ScraperAPI();
 
 export default function handler(req, res) {
+  if (!apiInstance.server) {
+    const { host, port } = apiInstance;
+
+    apiInstance.server = apiInstance.app.listen(port, host, () => {
+      console.log(`Serverless Express server started on http://${host}:${port}`);
+    });
+  }
+
   return apiInstance.app(req, res);
 }
 
